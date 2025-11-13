ecc8097a16cc7032620c0e0aa4afb5b18fc76464
koanasta <110098304+koanasta@users.noreply.github.com>
2025-11-13 13:07:03 +0200

    Add files via upload

---
+++ lab7/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
